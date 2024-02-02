a2e3799a0c5e739b1b1a722a402191c7ffd817ac
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-03 02:18:01 +0900

    #15 update:Head移設

--- src/pages/components/layout/MainLayout.tsx
+++ src/pages/components/layout/MainLayout.tsx
@@ -1,33 +1,43 @@
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { useTheme } from '@chakra-ui/react'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const theme = useTheme()
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        width: '100%',
-        backgroundColor: theme.colors.base[500],
-      }}
-    >
-      <main
-        className={inter.className}
+    <>
+      <Head>
+        <title>資産管理アプリ</title>
+        <meta name="description" content="資産管理アプリ" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
         style={{
           display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
           justifyContent: 'center',
           width: '100%',
-          maxWidth: '640px',
-          padding: '16px',
-          backgroundColor: theme.colors.base[50],
+          backgroundColor: theme.colors.base[500],
         }}
       >
-        {children}
-      </main>
-    </div>
+        <main
+          className={inter.className}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            maxWidth: '640px',
+            padding: '16px',
+            backgroundColor: theme.colors.base[50],
+          }}
+        >
+          {children}
+        </main>
+      </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,8 @@
-import Head from 'next/head'
 import { Button } from '@chakra-ui/react'
 import { MainLayout } from '@/pages/components/layout/MainLayout'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>資産管理アプリ</title>
-        <meta name="description" content="資産管理アプリ" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Button colorScheme="primary">Button</Button>
-    </>
-  )
+  return <Button colorScheme="primary">Button</Button>
 }
 
 Home.getLayout = (page: React.ReactNode) => <MainLayout>{page}</MainLayout>

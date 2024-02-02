725f096d07581f34e18e34ea9f7a616e6743a11a
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-03 01:56:42 +0900

    #15 update:レイアウトの調整

--- src/pages/components/layout/MainLayout.tsx
+++ src/pages/components/layout/MainLayout.tsx
@@ -1,10 +1,33 @@
 import { Inter } from 'next/font/google'
+import { useTheme } from '@chakra-ui/react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const theme = useTheme()
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-      <main className={inter.className}>{children}</main>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        width: '100%',
+        backgroundColor: theme.colors.base[500],
+      }}
+    >
+      <main
+        className={inter.className}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          maxWidth: '640px',
+          padding: '16px',
+          backgroundColor: theme.colors.base[50],
+        }}
+      >
+        {children}
+      </main>
     </div>
   )
 }

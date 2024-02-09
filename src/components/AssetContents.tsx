ce91c7c0b643518e091d9ef5a2ac5b383b8bb87f
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-09 16:10:11 +0900

    update:タイトル追加

--- src/components/AssetContents.tsx
+++ src/components/AssetContents.tsx
@@ -1,5 +1,6 @@
 import { CreateAssetModalButton } from '@/features/asset/CreateAssetModalButton'
 import { AssetListContainer } from '@/features/asset/AssetListContainer'
+import { Text } from '@chakra-ui/react'
 
 /**
  * 資産コンテンツ
@@ -8,6 +9,9 @@ import { AssetListContainer } from '@/features/asset/AssetListContainer'
 export default function AssetContents() {
   return (
     <>
+      <Text fontWeight="bold" fontSize="2xl">
+        資産一覧
+      </Text>
       <CreateAssetModalButton />
       <AssetListContainer />
     </>

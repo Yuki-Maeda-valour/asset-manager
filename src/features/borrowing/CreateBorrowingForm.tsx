4891a6bd4bd12cf4795c6d4fbd1141c1529d3d80
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-16 02:24:45 +0900

    #19 add:予約登録フォームInput追加

--- src/features/borrowing/CreateBorrowingForm.tsx
+++ src/features/borrowing/CreateBorrowingForm.tsx
@@ -13,6 +13,8 @@ import {
 } from '@/graphql/client/gqlhooks'
 import { AssetType } from '@/graphql/client/gqlhooks'
 import { useAssetForm } from '@/features/hooks/useAssetForm'
+import { LabelDateInput } from '@/components/input/LabelDateInput'
+import { LabelSelectInput } from '@/components/input/LabelSelectInput'
 
 export const CreateBorrowingForm = ({ onClose }: { onClose: () => void }) => {
   // const initialState = { name: '', type: AssetType.Pc }
@@ -39,15 +41,18 @@ export const CreateBorrowingForm = ({ onClose }: { onClose: () => void }) => {
   return (
     <form>
       <Container display="flex" flexDirection="column" w={'full'} gap={4}>
-        <Input type="text" name="name" placeholder="資産名" isRequired={true} />
-        <RadioGroup name="type">
-          <Stack display="flex" justifyContent="space-between" direction="row">
-            <Radio value={AssetType.Pc}>PC</Radio>
-            <Radio value={AssetType.Sp}>SP</Radio>
-            <Radio value={AssetType.Wifi}>WIFI</Radio>
-            <Radio value={AssetType.Monitor}>MONITOR</Radio>
-          </Stack>
-        </RadioGroup>
+        <LabelDateInput
+          label="予約日時"
+          value="2022-01-01T00:00:00+09:00"
+          isRequired={true}
+        />
+        <LabelDateInput
+          label="返却期限"
+          value="2022-01-01T00:00:00+09:00"
+          isRequired={true}
+        />
+        <LabelSelectInput label="予約者" value={1} isRequired={true} />
+        <LabelSelectInput label="物品" value={1} isRequired={true} />
         <Button type="submit">登録</Button>
       </Container>
     </form>

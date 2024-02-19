b0bb48c1d6d420675168447dceb093dd917590f5
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-19 18:44:52 +0900

    #19 update:編集機能の追加

--- src/features/utils/utils.ts
+++ src/features/utils/utils.ts
@@ -8,11 +8,21 @@ import { Maybe } from 'nexus/dist/core'
 export const formatDate = (dateString: Maybe<string> | undefined): string => {
   if (!dateString) return ''
   const date = new Date(dateString)
-  return new Intl.DateTimeFormat('ja-JP', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-  }).format(date)
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  return `${year}/${month}/${day} ${hours}:${minutes}`
+}
+
+export function toLocalDateTimeStringForInput(dateString: string) {
+  if (!dateString) return ''
+  const date = new Date(dateString)
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  return `${year}-${month}-${day}T${hours}:${minutes}`
 }

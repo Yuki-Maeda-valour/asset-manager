fbec096b93f3af2c08c580d9563c96f2f6798403
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2024-02-20 17:26:14 +0900

    #7 add:コンポーネントの仮設置

--- src/components/MainContents.tsx
+++ src/components/MainContents.tsx
@@ -2,17 +2,37 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel, Text } from '@chakra-ui/react'
 import AssetContents from '@/components/AssetContents'
 import UserContents from '@/components/UserContents'
 import BorrowingContents from '@/components/BorrowingContents'
+import { LoginButton } from '@/components/button/LoginButton'
+import { LogoutButton } from '@/components/button/LogoutButton'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '@/lib/firebase'
+import { useEffect } from 'react'
 
 /**
  * メインコンテンツ
  * @returns div
  */
 export default function MainContents() {
+  // ログイン状態を確認
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        console.log('ログインしていません')
+      } else {
+        console.log(`ログインしました: ${user.email} (${user.uid})`)
+      }
+    })
+  }, [])
+
   return (
     <>
       <Text fontWeight="bold" fontSize="2xl">
         資産予約管理アプリ
       </Text>
+      <div style={{ display: 'flex', justifyContent: 'space-between', gap: 8 }}>
+        <LoginButton />
+        <LogoutButton />
+      </div>
       <Tabs isFitted width="full">
         <TabList>
           <Tab>予約</Tab>
